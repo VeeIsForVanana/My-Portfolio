a593e32f14fd53c959a6d2d1d39ecdaf348b5927
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-02-28 23:41:34 +0800

    feat: modify base paths in config

--- svelte.config.js
+++ svelte.config.js
@@ -23,7 +23,11 @@ const config = {
 			fallback: undefined,
 			precompress: false,
 			strict: true
-		})
+		}),
+
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/my-portfolio' : '',
+		}
 	}
 };
 export default config;
