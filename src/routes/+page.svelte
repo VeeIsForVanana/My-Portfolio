58354e89347bf7683030dad93e3e4fc8d5933f9a
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-03-01 01:09:44 +0800

    feat: add transition to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
 <script lang="ts">
-	import { fade } from 'svelte/transition';
+	import { fly } from 'svelte/transition';
 
 	export const data = {};
-	let visible_secondary_header = true;
+	let visible_secondary_header = false;
+
+	setTimeout(() => {visible_secondary_header = true}, 200)
 
 </script>
 
@@ -12,7 +14,7 @@
 	<div class="space-y-5">
 		<h1 class="h1">Hi, I'm <span class="text-secondary-500">Victor</span>!</h1>
 		{#if visible_secondary_header}
-			<h3 transition:fade={{duration: 500}}>Welcome to my portfolio.</h3>
+			<h3 class="h5" transition:fly={{duration: 350, y:-100}}>Welcome to my portfolio.</h3>
 		{/if}
 	</div>
 </div>
