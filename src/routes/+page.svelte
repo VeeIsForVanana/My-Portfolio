fcbb6167d51be7a706b40da6c6c8670d8d95ce6c
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-03-01 02:00:33 +0800

    fix: fix jumping headers due to transition

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 	import { fly } from 'svelte/transition';
 
 	export const data = {};
-	let visible_secondary_header = false;
+	$: visible_secondary_header = false;
 
 	setTimeout(() => {visible_secondary_header = true}, 200)
 
@@ -19,9 +19,9 @@
 
 <div class="container h-full flex justify-center items-center mx-20 max-w-screen-lg">
 	<div class="space-y-5">
-		<h1 class="h1">Hi, I'm <span class="text-secondary-500">Victor</span>!</h1>
+		<h1 class="h1" style={visible_secondary_header ? "margin-bottom:0px" : "margin-bottom:68px"}>Hi, I'm <span class="text-secondary-500">Victor</span>!</h1>
 		{#if visible_secondary_header}
-			<h3 class="h5" transition:fly={{duration: 350, y:-100}}>Welcome to my portfolio.</h3>
+			<h3 class="h5 mb-1" transition:fly={{duration: 350, y:-100}}>Welcome to my portfolio.</h3>
 		{/if}
 		<p>I am a student at the University of the Philippines Diliman currently undergoing the <span class="text-secondary-600">Bachelor of Science in Computer Science</span> program (Batch 2021, expected graduation in 2025). This portfolio is intended to showcase my projects, skills, and experience as a developer, along with a few other things.</p>
 	</div>
