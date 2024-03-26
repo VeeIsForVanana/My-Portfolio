9ac3df61e19ec09184da07a1c251ce9dc9b5d292
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-03-26 19:15:15 +0800

    feat: add new section to about-me

--- src/routes/about-me/+page.svelte
+++ src/routes/about-me/+page.svelte
@@ -63,7 +63,7 @@
 
         <p>Now, this is quite a lot. It is definitely much more than what other institutions teach, and much more than is needed to make a developer or a programmer. I've heard many a fellow student ask themselves why we even need all of this. I even asked myself this as I stumbled through our infamous low-level Systems Series and our woefully abstract Discrete Math subjects.</p>
 
-        <p>Now; however, three years into the program, I understand why we're being trained this way. It is all, simply enough, in the name. Our course does not raise developers or programmers. I did not go through this course just so at the end I could say "I am a coder". The end goal of this course, and for me in this course, is for me to be able to say:</p>
+        <p>Now; however, three years into the program, I understand why we're being trained this way. It is all, simply enough, in the name. Our course does not raise developers or programmers. I did not go through this course just so at the end I could say "I am a coder". The end goal of this course, and for me in this curriculum, is for me to be able to say:</p>
 
         <h3 class="text-tertiary-700 drop-shadow-md">I am a Computer Scientist</h3>
 
@@ -71,14 +71,24 @@
 
         <h3>I am an Explorer</h3>
 
-        <p>I don't really confine my learning to a classroom. Of course, there's the obvious school of life schtick - life always teaches new things about you and the world around you. In addition to this, though, I've always had a habit of digging myself into the strangest and most interesting rabbitholes. </p>
+        <p>I don't really confine my learning to a classroom. Of course, there's the obvious school of life schtick - life always teaches new things about you and the world around you. In addition to this, though, I've always had a habit of digging myself into the strangest and most interesting rabbitholes beyond the context of my academic interests. </p>
 
         <!--Insert some trivia switcher thing here with interesting fun facts or something-->
+
         
         <p>In the past, I've explored such fields as <span class="text-primary-500">{#each list_of_topics as topic} {topic}{list_of_topics.findIndex((elem) => elem == topic) != list_of_topics.length ? ', ' : ''} {/each}</span> among many and varied others. You will, of course, ask why. My answer is that I just think they're all interesting. At the same time, these topics have been of use to me or have influenced the way I think.</p>
 
-        <p></p>
+        <p>For example, my exploration of geography has given me an appreciation for how different places raise different people, with different worldviews and different skills, all equally valuable, profound, and useful. Meanwhile, my forays into linguistics and dabbling in foreign languages has given me an appreciation of the languages I already speak, while presenting me with a good jumping-off point in case I decide to actually learn a new language.</p>
 
+        <p>A lot of these 'extracurriculars' were actually born out of my hobbies. I've studied Game Design because I like trying to develop games as a side hobby. I've studied geography and history in part because they are interesting in and of themselves, but also because I build historical worlds as a hobby (referred to as "worldbuilding" in some circles). In fact, a lot of my extra-academic fields of interests were born because I wanted to imbue the worlds I made with some sense of realism and groundedness in reality.</p>
+
+        <p>Looking closer at my interests, I guess one common theme is the idea that I like collecting <span class="text-primary-500">stories</span>. In fact, even in my academics one of my favorite parts of learning is understanding how all the various parts of the systems and structures I learn come together. There is, of course, the mechanistic pleasure in watching everything come together as a puzzle. But I've always maintained a keen interest in watching the patterns, and sometimes, the stories that they tell. Whether it be in observing the way design decisions in operating systems have evolved over time and left their mark as strange artifacts and curiosities on modern systems, or by understanding the delicate interplay and balance within complex systems like political systems, human infrastructure, and living ecologies.</p>
+
+        <p>Of course, all of these hobbies, all of this knowledge, does not (and should not) exist simply for my own indulgence. In all my forays, all my expeditions into the furthest corners of human knowledge, there is always the knowledge that I will return more than I was when I left, to help others become more than they are, with the stories and knowledge I bear. In other words,</p>
+
+        <h3 class = "text-tertiary-700 drop-shadow-md">I am a Storyteller</h3>
+
+        <hr>
     </div> 
 </div>
 
